9dead954d43e0a41e2e1b423fe98c4ac7a72d48e
PrivateRouter <101918538+PrivateRouter@users.noreply.github.com>
2023-09-28 22:05:16 -0400

    Update autoprovision-stage2.sh

--- files/etc/auto-provision/autoprovision-stage2.sh
+++ files/etc/auto-provision/autoprovision-stage2.sh
@@ -87,7 +87,7 @@ installPackages()
     opkg install tgrouterappstore luci-app-shortcutmenu luci-app-poweroff luci-app-wizard luci-app-openwisp
     opkg remove wpad wpad-basic wpad-basic-openssl wpad-basic-wolfssl wpad-wolfssl openwisp-monitoring openwisp-config
     opkg install wpad-mesh-openssl kmod-batman-adv batctl avahi-autoipd batctl-full luci-app-dawn mesh11sd
-    opkg install /etc/luci-app-easymesh_2.2_all.ipk
+    opkg install /etc/luci-app-easymesh_2.3_all.ipk
     opkg install /etc/luci-proto-batman-adv_git-22.104.47289-0a762fd_all.ipk
     
     # List of our packages to install
